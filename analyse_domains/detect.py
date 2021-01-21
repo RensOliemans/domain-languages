666633ac9c7808b128b57d152382405551e640d4
Rens Oliemans <hallo@rensoliemans.nl>
2021-01-21 01:04:52 +0100

    :sparkles: Finish detect for small files

--- analyse_domains/detect.py
+++ analyse_domains/detect.py
@@ -1,3 +1,5 @@
+from multiprocessing.pool import ThreadPool
+
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
@@ -19,23 +21,26 @@ pipeline = PretrainedPipeline(PIPELINE[0], lang=PIPELINE[1])
 COUNTRIES = ['de', 'fr', 'es', 'uk', 'se', 'it', 'ru', 'gr']
 LANGUAGES = ['bg', 'cs', 'de', 'el', 'en', 'es', 'fi', 'fr', 'hr', 'hu',
              'it', 'no', 'pl', 'pt', 'ro', 'ru', 'sk', 'sv', 'tr', 'uk']
-COUNTRY = 'fr'
-df = spark.read.option('header', 'true').csv('small_' + COUNTRY)
 
 
-# def detect(content):
-#     return pipeline.annotate(content)['language'][0]
+def main(country):
+    print('country')
+    try:
+        df = spark.read.option('header', 'true').csv('small_' + country)
+    except Exception:
+        print(f'No files found under country {country}')
+        return
+
+    rdd = df.rdd
+    rdd = rdd.map(lambda ccu: (ccu[1], detect(ccu[0])))
+    rdd = rdd.map(lambda cl: (cl[0], {lang: 1 if lang == cl[1] else 0 for lang in LANGUAGES}))
+    rdd = rdd.reduceByKey(lambda a, b: {c: a[c] + b[c] for c in LANGUAGES})
+
+    print(f'For country: {country}, results: {rdd.collect}')
 
 
-rdd = df.rdd
-rdd = rdd.map(lambda ccu: (ccu[1], detect(ccu[0])))
-rdd = rdd.map(lambda cl: (cl[0], {lang: 1 if lang == cl[1] else 0 for lang in LANGUAGES}))
-rdd = rdd.reduceByKey(lambda a, b: {c: a[c] + b[c] for c in LANGUAGES})
-
-print(rdd.collect())
-
-# tuple unpacking
-# rdd = df.rdd \
-#     .map(lambda (content, country, url): (country, pipeline.annotate(content)['language'][0])) \
-#     .map(lambda (country, language): (country, {lang: 1 if lang == language else 0 for lang in LANGUAGES})) \
-#     .reduceByKey(lambda a, b: {c: a[c] + b[c] for c in LANGUAGES})
+if __name__ == '__main__':
+    p = ThreadPool(8)
+    p.map(main, COUNTRIES)
+    p.close()
+    p.join()
