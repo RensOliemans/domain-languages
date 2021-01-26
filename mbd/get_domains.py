5605ee45b5e734208352424e40e7ed327e67cb51
Rens Oliemans <hallo@rensoliemans.nl>
2021-01-26 10:57:26 +0100

    :sparkles: Get gz files

--- mbd/get_domains.py
+++ mbd/get_domains.py
@@ -2,6 +2,15 @@ import os
 
 import requests
 
+# from pyspark import SparkContext
+# from pyspark.sql import SparkSession
+#
+#
+# APPNAME = 'Download CC GZs'
+# sc = SparkContext(appName=APPNAME)
+# sc.setLogLevel('ERROR')
+# spark = SparkSession.builder.appName(APPNAME).getOrCreate()
+
 
 class DomainGetter:
     def __init__(self, prefix, instance, cluster_filename):
@@ -51,7 +60,7 @@ def get_file_urls(prefix, instances, pattern):
     save_cluster_files(prefix, instances)
 
     for instance in instances:
-        print 'Getting urls of cluster %s' % instance
+        print('Getting urls from cluster %s with pattern %s' % (instance, pattern))
         dg = DomainGetter(prefix, instance, 'clusters/cluster-{}}.idx'.format(instances))
         yield dg.get_urls(pattern)
 
@@ -61,17 +70,36 @@ def save_cluster_files(prefix, instances):
         full_instance = 'CC-MAIN-' + instance
         cfg = ClusterFileSaver(prefix, full_instance, 'clusters/', 'cluster-{}.idx'.format(instance))
         if not cfg.exists:
-            print 'Saving file %s' % full_instance
+            print('Saving file %s' % full_instance)
             cfg.save()
+        else:
+            print('File %s already existed' % full_instance)
+
+
+def get_gz_files(file_urls):
+    for file_url in file_urls:
+        parts = file_url.split('/')
+        filename = parts[-1]
+        instance = parts[-2]
+        out_filename = '{}--{}'.format(instance, filename)
+        with requests.get(file_url, stream=True) as r:
+            r.raise_for_status()
+            with open(out_filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        yield out_filename
 
 
 def download_files(pattern):
     prefix = 'https://commoncrawl.s3.amazonaws.com/'
-    instances = ['2020-24', '2018-22', '2017-22', '2016-22']
+    instances = ['2020-50']
 
+    print('Getting file urls, possibly downloading cluster files for it')
     file_urls = get_file_urls(prefix, instances, '{},'.format(pattern))
-    
 
+    print('Getting gz_files')
+    gz_filenames = get_gz_files(file_urls)
+    print(list(gz_filenames))
 
 
 if __name__ == '__main__':
