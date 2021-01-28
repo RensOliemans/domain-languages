df2a42d9001a5c0e234c9c85ab49795a682b3b37
Rens Oliemans <hallo@rensoliemans.nl>
2021-01-28 10:48:02 +0100

    :pencil2: Add get warc data

---
+++ mbd/get_warc_data.py
@@ -0,0 +1,35 @@
+import requests
+
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+
+import pyspark.sql.functions as F
+from pyspark.sql.types import *
+
+
+APPNAME = 'Get WARCs'
+sc = SparkContext(appName=APPNAME)
+sc.setLogLevel('ERROR')
+spark = SparkSession.builder.appName(APPNAME).getOrCreate()
+
+
+LANGUAGE = 'fr'
+INSTANCE = '2020-50'
+directory = 'output2/CC-MAIN-{}-{}'.format(INSTANCE, LANGUAGE)
+
+PREFIX = 'https://commoncrawl.s3.amazonaws.com/'
+
+df = spark.read.option('header', 'true').csv(directory)
+filenames = df.filename.collect()
+
+for i, filename in enumerate(filenames):
+    url = PREFIX + filename
+    out_filename = i + '.warc.gz'
+
+    with requests.get(url, stream=True) as r:
+        print('Downloading file %s' % url)
+        r.raise_for_status()
+        with open(out_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    print('Written file %s' % out_filename)
