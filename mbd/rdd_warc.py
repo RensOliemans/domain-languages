352181bc8ecc983a4c8047bca2e0135af189c8cd
Rens Oliemans <hallo@rensoliemans.nl>
2021-01-28 14:47:03 +0100

    :sparkles: Full dataset

--- mbd/rdd_warc.py
+++ mbd/rdd_warc.py
@@ -123,7 +123,7 @@ PREFIX = 'https://commoncrawl.s3.amazonaws.com/'
 logging.info('Reading 1%% of %s', directory)
 schema = ['tld', 'content']
 
-df = spark.read.option('header', 'true').csv(directory).sample(0.01)
+df = spark.read.option('header', 'true').csv(directory)
 rdd = df.rdd.map(lambda row: download_row(row)).filter(bool)
 df = spark.createDataFrame(rdd, schema)
 df.write.format('parquet').mode('overwrite').option('header', 'true').csv('output/{}-{}'.format(INSTANCE, LANGUAGE))
